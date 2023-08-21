c34ea65e843a5b9d6601ffad5e2e0d56fb5daa5b
paul <paulkaf84@gmail.com>
2023-08-21 19:43:47 +0200

    base

---
+++ package/cli/src/cli.c
@@ -0,0 +1,18 @@
+//
+// Created by lux on 8/16/23.
+//
+
+#include <stdio.h>
+#include "include/cli.h"
+
+void printColorText(char * format, void *content, Color color) {
+    printf("\033[1;%dm", color);
+    printf(format, content);
+    printf("\033[0m");
+}
+
+void printColorTextLn(char * format, void *content, Color color) {
+    printf("\033[1;%dm", color);
+    printf(format, content);
+    printf("\033[0m\n");
+}
